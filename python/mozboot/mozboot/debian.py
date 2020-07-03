fee15a6b42980a96c7747c49f8db5c091eccffa4
Pip Cet <pipcet@gmail.com>
2020-07-03 18:15:45 +0000

    python/mozboot/mozboot/debian.py: skip broken Java check

--- python/mozboot/mozboot/debian.py
+++ python/mozboot/mozboot/debian.py
@@ -154,7 +154,6 @@ class DebianBootstrapper(
         self.apt_install(*self.mobile_android_packages)
 
         # 2. Android pieces.
-        self.ensure_java()
         from mozboot import android
         android.ensure_android('linux', artifact_mode=artifact_mode,
                                no_interactive=self.no_interactive)
